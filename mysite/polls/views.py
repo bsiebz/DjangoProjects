0836e80c86917fa5f30dc09f0b7792fabd1b48ce
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-09-27 17:15:25 -0700

    Cleaning up the code and fixing errors.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-Class IndexView(generic.ListView):
+class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-    """Return the last five published questions."""
-    return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
     
-Class DetailView(generic.DetailView):
+class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
     
-Class ResultsView(generic.DetailView):
+class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html' 
   
