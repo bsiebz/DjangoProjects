ccb7ffc6007fdeb075b4bc2d4ab061ff2e13fb7b
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-09-27 18:16:53 -0700

    Adding more test cases.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -15,6 +15,26 @@ def create_question(question_text, days):
     time = timezone.now() +datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
+class QuestionIndexDetailTest(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question= create_question(question_text="Future question.", days=5)
+        response = self.client.get(reverse('polls:detail',args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+        
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text="Past question.", days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text, status_code=200)
+
+
 class QuestionViewTests(TestCase):
     def test_index_view_with_no_questions(self):
         """
@@ -75,8 +95,7 @@ class QuestionViewTests(TestCase):
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
-        )
-              
+        )      
         
 class QuestionMethodTests(TestCase):
 
